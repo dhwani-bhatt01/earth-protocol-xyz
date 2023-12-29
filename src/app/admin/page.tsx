30da438f826920ae22e9f1785c5fc7431c82618f
dhwani bhatt <bhattdhwani01@gmail.com>
2023-12-29 15:59:51 +0530

    created a new abi file and exported it

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,13 +1,16 @@
 "use client";
 
-import earthLpStaking from "@/abi/EarthLpStaking.sol/EarthLpStaking.json";
+// import earthLpStaking from "@/abi/EarthLpStaking.sol/EarthLpStaking.json";
+import ABI from "@/abi/EarthLpStaking.sol/Abi.json";
 import { ethers } from "ethers";
 import { Toast } from "primereact/toast";
 import { useRef } from "react";
-import { useWalletClient } from "wagmi";
 import Styles from "./admin.module.css";
 
 export default function Admin() {
+	const privateKey =
+		"0xfc2f8cc0abd2d9d05229c8942e8a529d1ba9265eb1b4c720c03f7d074615afbb";
+
 	const parentStrategyValContract =
 		// "0x46ECf770a99d5d81056243deA22ecaB7271a43C7";
 		"0x86E6b3c84eaaDa895017b5ad1A44e9ea63c3cCe5";
@@ -17,7 +20,9 @@ export default function Admin() {
 		"0xEd16712bEaD2b6eed8cd514F8fB8a0151CCb8689",
 		"0x9974DA8Cb3cb6C4b5121aE25FD87A8a0F3cB544b",
 	];
-	const { data: signer } = useWalletClient();
+	const provider = new ethers.JsonRpcProvider("https://node.rivera.money/"); // Update with your local node URL
+	const signer = new ethers.Wallet(privateKey, provider);
+
 	const toast = useRef<Toast>(null);
 
 	const showError = (message: string) => {
@@ -38,17 +43,19 @@ export default function Admin() {
 		});
 	};
 
-	const getContract = (address: string, abi: any, provider: any) => {
-		return new ethers.Contract(address, abi, provider);
+	const getContract = (address: string, abi: any, signer: any) => {
+		return new ethers.Contract(address, abi, signer);
 	};
 
 	const startEpochFun = async () => {
 		const parentStrategy = getContract(
 			parentStrategyValContract as string,
-			earthLpStaking.abi,
+			// earthLpStaking.abi,
+			ABI,
 			signer
 		);
 		const startTxt = await parentStrategy.startEpoch(vaultContractList);
+
 		await startTxt
 			.wait()
 			.then(async (e: any) => {
@@ -62,7 +69,8 @@ export default function Admin() {
 	const endEpochFun = async () => {
 		const parentStrategy = getContract(
 			parentStrategyValContract as string,
-			earthLpStaking.abi,
+			// earthLpStaking.abi,
+			ABI,
 			signer
 		);
 		const endTxt = await parentStrategy.endEpoch();
